170a9001bea926a6cc3d825f7b584c10d79a2354
Michel <michel@cludus.org>
2026-07-08 16:46:57 -0500

    Add initial download images script and vms.yml test configuration

---
+++ bin/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
+root_dir=$(pwd)
+
+# TODO: extract version
+curl https://dl-cdn.alpinelinux.org/alpine/v3.24/releases/cloud/generic_alpine-3.24.1-x86_64-bios-cloudinit-r0.qcow2 -o "$root_dir/imgs/alpine.img"
+curl https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img -o "$root_dir/imgs/ubuntu.img"
